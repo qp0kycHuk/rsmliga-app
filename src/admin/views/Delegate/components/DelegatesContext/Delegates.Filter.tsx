bdc94eec0164b35efddb72ff761301c516059d2d
qp0kycHuk <serega990112@mail.ru>
2023-09-11 09:11:19 +0300

    change filter

--- src/admin/views/Delegate/components/DelegatesContext/Delegates.Filter.tsx
+++ src/admin/views/Delegate/components/DelegatesContext/Delegates.Filter.tsx
@@ -1,36 +1,16 @@
-import { CirclePlusIcon } from '@assets/icons/fill'
-import { Dialog, Button } from '@features/ui'
-import { useToggle } from '@hooks/useToggle'
-import { Suspense } from 'react'
-import { DelegateEdit } from '../DelegateEdit/DelegateEdit'
 import { Search } from './Delegates.Search'
 import { Seasons } from './Delegates.Filter.Seasons'
 import { Tournaments } from './Delegates.Filter.Tournaments'
 import { Stages } from './Delegates.Filter.Stages'
 
-export function Filter() {
-  const [isDialogOpen, , openDialog, closeDialog] = useToggle(false)
-
+export function Filter({ children }: React.PropsWithChildren) {
   return (
-    <>
-      <div className="flex items-center gap-10 mb-6 ">
-        <Seasons />
-        <Tournaments />
-        <Stages />
-
-        <Search />
-
-        <Button variant="text" onClick={openDialog} className="gap-3 font-semibold">
-          <CirclePlusIcon className="text-2xl" />
-          Добавить судью
-        </Button>
-      </div>
-
-      <Dialog isOpen={isDialogOpen} onClose={closeDialog} className="container p-10">
-        <Suspense fallback="Loading...">
-          <DelegateEdit />
-        </Suspense>
-      </Dialog>
-    </>
+    <div className="flex items-center gap-10 mb-6 ">
+      <Seasons />
+      <Tournaments />
+      <Stages />
+      <Search />
+      {children}
+    </div>
   )
 }
--- src/admin/views/Delegate/components/List/List.tsx
+++ src/admin/views/Delegate/components/List/List.tsx
@@ -1,3 +1,7 @@
+import { Suspense } from 'react'
+import { Button, Dialog } from '@features/ui'
+import { CirclePlusIcon } from '@assets/icons/fill'
+import { useToggle } from '@hooks/useToggle'
 import { ListTable } from './List.Table'
 import {
   DelegatesContextProvider,
@@ -5,17 +9,30 @@ import {
 } from '../DelegatesContext/Delegates.Context'
 import { Pagination } from '../DelegatesContext/Delegates.Pagination'
 import { Filter } from '../DelegatesContext/Delegates.Filter'
+import { DelegateEdit } from '../DelegateEdit/DelegateEdit'
 
 function ListInner() {
+  const [isDialogOpen, , openDialog, closeDialog] = useToggle(false)
   const { loading, delegates } = useDelegatesContext()
 
   return (
     <>
       <div className="mb-5 text-3xl font-bold">Список</div>
-      <Filter />
+      <Filter>
+        <Button variant="text" onClick={openDialog} className="gap-3 font-semibold">
+          <CirclePlusIcon className="text-2xl" />
+          Добавить судью
+        </Button>
+      </Filter>
       {loading ? 'loading...' : <ListTable items={delegates} />}
 
       <Pagination />
+
+      <Dialog isOpen={isDialogOpen} onClose={closeDialog} className="container p-10">
+        <Suspense fallback="Loading...">
+          <DelegateEdit />
+        </Suspense>
+      </Dialog>
     </>
   )
 }
