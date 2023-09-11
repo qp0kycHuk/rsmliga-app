ea91870752fa31ae72d68d7a23a666e44794534a
qp0kycHuk <serega990112@mail.ru>
2023-09-11 09:07:19 +0300

    add user info

--- src/admin/layouts/Header/Header.tsx
+++ src/admin/layouts/Header/Header.tsx
@@ -2,21 +2,31 @@ import { LogOutIcon } from '@assets/icons/fill'
 import { Avatar, Button } from '@features/ui'
 import classes from './Header.module.scss'
 import classnames from 'classnames'
+import { useFetchCurrentUser } from '@admin/service/user'
 
 export function Header() {
+  const { data } = useFetchCurrentUser()
+
   return (
     <header className={classnames(classes.header, 'flex items-center py-2')}>
       <div className={classnames(classes.user, 'flex items-center gap-2')}>
-        <Avatar src="/img/test.gif" />
+        <Avatar src={data?.item.imgPath} />
 
-        <div className="text-body-2 text--demibold">Евгения Лиссова</div>
+        <div className="text-body-2 text--demibold">{data?.item.name}</div>
 
-        <Button className="ml-auto rounded-lg" icon size="sm" variant="light">
+        <Button
+          as="a"
+          href={data?.item.logoutUrl}
+          className="ml-auto rounded-lg"
+          icon
+          size="sm"
+          variant="light"
+        >
           <LogOutIcon className="text-lg" />
         </Button>
       </div>
 
-      <Button className="px-10 ml-auto" size="xs">
+      <Button as="a" href="/" className="px-10 ml-auto" size="xs">
         На сайт
       </Button>
     </header>
