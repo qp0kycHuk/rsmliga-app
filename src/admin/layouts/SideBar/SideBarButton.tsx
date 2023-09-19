8872abb1b38906471ac93a38c7233024b6c95923
qp0kycHuk <serega990112@mail.ru>
2023-09-19 09:51:13 +0300

    add sidebar tooltip

--- src/admin/layouts/SideBar/SideBarButton.tsx
+++ src/admin/layouts/SideBar/SideBarButton.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import { Button } from '@features/ui'
+import { Button, Tooltip } from '@features/ui'
 import { Link } from '@lib/Link'
 import { useSidebarContext } from './SidebarContext'
 
@@ -18,16 +18,21 @@ export function SideBarButton({ title, icon: Icon, to, sub }: IProps) {
   }
 
   return (
-    <Button
-      onClick={closeSidebar}
-      as={Link}
-      href={to}
-      to={to}
-      className={classNames('justify-start w-full  mb-1 whitespace-nowrap', sub ? 'pl-10' : 'pl-3')}
-      variant="none"
-    >
-      <Icon className={classNames('flex-shrink-0 ', sub ? 'text-xs' : 'text-2xl')} />
-      {isOpen && <div className="ml-2 text-base text-black">{title}</div>}
-    </Button>
+    <Tooltip content={!isOpen ? title : null} placement="right" disabled={isOpen}>
+      <Button
+        onClick={closeSidebar}
+        as={Link}
+        href={to}
+        to={to}
+        className={classNames(
+          'justify-start w-full  mb-1 whitespace-nowrap',
+          sub ? 'pl-10' : 'pl-3'
+        )}
+        variant="none"
+      >
+        <Icon className={classNames('flex-shrink-0 ', sub ? 'text-xs' : 'text-2xl')} />
+        {isOpen && <div className="ml-2 text-base text-black">{title}</div>}
+      </Button>
+    </Tooltip>
   )
 }
--- src/lib/Link.tsx
+++ src/lib/Link.tsx
@@ -1,10 +1,14 @@
+import React from 'react'
 // export { Link } from 'react-router-dom'
 
 type ILinkProps = React.DetailedHTMLProps<
   React.AnchorHTMLAttributes<HTMLAnchorElement>,
   HTMLAnchorElement
 >
+type LinkRef = React.ForwardedRef<HTMLAnchorElement>
 
-export function Link(props: ILinkProps) {
-  return <a {...props} />
+export function LinkComponent(props: ILinkProps, ref: LinkRef) {
+  return <a ref={ref} {...props} />
 }
+
+export const Link = React.forwardRef(LinkComponent)
