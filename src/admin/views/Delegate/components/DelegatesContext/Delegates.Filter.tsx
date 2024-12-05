46957b2b35390427fc3ad4362ede6c2c39c91f30
Александр Актищев <aktishch@gmail.com>
2024-12-05 12:09:20 +0300

    delegates filtering print

--- src/admin/views/Delegate/components/DelegatesContext/Delegates.Filter.tsx
+++ src/admin/views/Delegate/components/DelegatesContext/Delegates.Filter.tsx
@@ -5,7 +5,7 @@ import { Stages } from './Delegates.Filter.Stages'
 
 export function Filter({ children }: React.PropsWithChildren) {
   return (
-    <div className="flex gap-5 mb-10 lg:items-center max-lg:flex-col lg:gap-10 lg:mb-6 print:hidden">
+    <div className="flex gap-5 mb-10 lg:items-center max-lg:flex-col lg:gap-10 lg:mb-6">
       <div className="flex gap-4 md:items-center md:gap-10 max-md:flex-col ">
         <Seasons />
         <Tournaments />
